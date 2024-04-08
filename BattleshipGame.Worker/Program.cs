7a0ad126d086e7b77784e8a0b2a9f46bad0f6fd9
rodrigolessa <rodrigolsr@gmail.com>
2024-04-08 10:18:37 -0300

    :tada: The project scaffold

---
+++ BattleshipGame.Worker/Program.cs
@@ -0,0 +1,7 @@
+using BattleshipGame.Worker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .Build();
+
+host.Run();
