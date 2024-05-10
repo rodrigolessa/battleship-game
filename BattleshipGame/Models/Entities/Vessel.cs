f01609f5e61859f0e8756df56b5575218cdd8b3c
rodrigolessa <rodrigolsr@gmail.com>
2024-05-10 19:37:07 -0300

    :recycle: Move entities to its own namespace

--- BattleshipGame/Models/Entities/Vessel.cs
+++ BattleshipGame/Models/Entities/Vessel.cs
@@ -1,8 +1,9 @@
-﻿using BattleshipGame.Models.Enumerations;
+﻿using BattleshipGame.Infrastructure.Models;
+using BattleshipGame.Models.Enumerations;
 
 namespace BattleshipGame.Models.Entities;
 
-public abstract class Vessel
+public abstract class Vessel : Entity
 {
     public string Name { get; set; }
     public int Width { get; set; }
--- BattleshipGame/Models/Entities/Vessels/Destroyer.cs
+++ BattleshipGame/Models/Entities/Vessels/Destroyer.cs
@@ -1,7 +1,7 @@
 using BattleshipGame.Models.Enumerations;
 using Humanizer;
 
-namespace BattleshipGame.Models.Entities;
+namespace BattleshipGame.Models.Entities.Vessels;
 
 public class Destroyer : Vessel
 {
