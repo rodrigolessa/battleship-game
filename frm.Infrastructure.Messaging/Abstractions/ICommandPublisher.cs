3ff23f83d28c46b06d9118a0093e3e17dad0064b
rodrigolessa <rodrigolsr@gmail.com>
2025-07-15 22:39:56 -0300

    Add RabbitMQ publisher

--- frm.Infrastructure.Messaging/Abstractions/ICommandPublisher.cs
+++ frm.Infrastructure.Messaging/Abstractions/ICommandPublisher.cs
@@ -6,5 +6,7 @@ public interface ICommandPublisher
 {
     Task PublishAsync(IBaseCommand command, string channelKey = "", int? delayInSeconds = null, CancellationToken cancellationToken = default);
     Task PublishAsync<T>(List<T> commands, string channelKey = "", int? delayInSeconds = null) where T : IBaseCommand;
+    Task PublishAsync<T>(T message, string channelKey = "", CancellationToken cancellationToken = default);
+    // Send later (delayed messages using TTL + dead-letter exchange pattern)
     Task ScheduleAsync<T>(T message, DateTime scheduledTimeUtc, string channelKey = "", CancellationToken cancellationToken = default);
 }
