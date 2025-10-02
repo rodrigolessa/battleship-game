bc8b83e25b1025f7a5fd384e76afb85aa71c4181
rodrigolessa <rodrigolsr@gmail.com>
2025-10-02 15:24:51 -0300

    Add optimistic control

--- Databases/EventStoreDb/02 - Table - GameEvent.sql
+++ Databases/EventStoreDb/02 - Table - GameEvent.sql
@@ -2,7 +2,7 @@ CREATE TABLE [eventstore].[GameEvent] (
     [EventKey] CHAR(36) NOT NULL, -- PRIMARY KEY
     [EventType] VARCHAR(MAX) NULL,
     [EventVersion] TINYINT NULL,
-    [EventSequence] INT NOT NULL, -- Optimistic concurrency. To avoid dirty read and other problem of Race Conditions, we only persist data if the sequence is the same as we read before. Conditional UPDATE and in this case will be a conditional INSERT
+    [EventSequence] INT NOT NULL, -- Optimistic concurrency control (OCC). Concurrency token. To avoid dirty read and other problem of Race Conditions, we only persist data if the sequence is the same as we read before. Conditional UPDATE and in this case will be a conditional INSERT
     [EventCommittedTimestamp] DATETIME2 (7) NOT NULL,
     [AggregateId] CHAR (36) NOT NULL, -- Lexicographical ordering
     [IdempotencyKey] CHAR(36) NOT NULL, -- Duplicate event detection (idempotency key). Should be unique
@@ -10,3 +10,6 @@ CREATE TABLE [eventstore].[GameEvent] (
     [SagaProcessKey] CHAR(36) NULL
     CONSTRAINT [PK_GameEvent] PRIMARY KEY CLUSTERED ([EventKey] ASC)
 );
+
+ALTER TABLE [eventstore].[GameEvent]
+    ADD CONSTRAINT UQ_Aggregate_Sequence UNIQUE (AggregateId, EventSequence);
